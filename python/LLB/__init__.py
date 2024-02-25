e0aa86091721fafb9be54e24544803e9b2f11909
BopaxDev <marionicdematteis@gmail.com>
2024-02-25 11:36:42 -0600

    Add tests

---
+++ python/LLB/__init__.py
@@ -0,0 +1,6 @@
+from .LLB import *
+
+
+__doc__ = LLB.__doc__
+if hasattr(LLB, "__all__"):
+    __all__ = LLB.__all__
---
+++ python/tests/test_all.py
@@ -0,0 +1,6 @@
+import pytest
+import LLB
+
+
+def test_sum_as_string():
+    assert LLB.sum_as_string(1, 1) == "2"
